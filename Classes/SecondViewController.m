51e7c28c16ce35e64fc6b891b46d4c86cd9247f9
Shane Zatezalo <szatezalo@Malachi.local>
2010-03-16 13:59:13 -0400

    works if you do it through code.

--- Classes/SecondViewController.m
+++ Classes/SecondViewController.m
@@ -22,34 +22,35 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
-//	window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-//	[window setBackgroundColor:[UIColor yellowColor]];
-//	
-//	NSMutableArray *localControllersArray = [[NSMutableArray alloc] initWithCapacity:2];
-//
-//	if (self.tabBarFirstTab == nil) {
-//		TabBarFirstTabViewController *tempFirstTab = [[TabBarFirstTabViewController alloc] init];
-//		self.tabBarFirstTab = tempFirstTab;
-//		[tempFirstTab release];
-//	}
-//	[localControllersArray addObject:self.tabBarFirstTab];
-//
-//	if (self.tabBarSecondTab == nil) {
-//		TabBarSecondTabViewController *tempSecondTab = [[TabBarSecondTabViewController alloc] init];
-//		self.tabBarSecondTab = tempSecondTab;
-//		[tempSecondTab release];
-//	}
-//	[localControllersArray addObject:self.tabBarSecondTab];
-//	
-//	UITabBarController *tempTBC = [[UITabBarController alloc] initWithNibName:@"SecondViewController" bundle:[NSBundle mainBundle]];
-//	tempTBC.viewControllers = localControllersArray;
-//	[localControllersArray release];
-//	self.tabBar = tempTBC;
-//	[tempTBC release];
-//	NSLog(@"tempVC");
-//	
-//	[window addSubview:self.tabBarController.view];
-//	[window makeKeyAndVisible];
+	window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+	[window setBackgroundColor:[UIColor yellowColor]];
+	
+	NSMutableArray *localControllersArray = [[NSMutableArray alloc] initWithCapacity:2];
+
+	if (self.tabBarFirstTab == nil) {
+		TabBarFirstTabViewController *tempFirstTab = [[TabBarFirstTabViewController alloc] init];
+		self.tabBarFirstTab = tempFirstTab;
+		[tempFirstTab release];
+	}
+	[localControllersArray addObject:self.tabBarFirstTab];
+
+	if (self.tabBarSecondTab == nil) {
+		TabBarSecondTabViewController *tempSecondTab = [[TabBarSecondTabViewController alloc] init];
+		self.tabBarSecondTab = tempSecondTab;
+		[tempSecondTab release];
+	}
+	[localControllersArray addObject:self.tabBarSecondTab];
+	
+	UITabBarController *tempTBC = [[UITabBarController alloc] 
+								   initWithNibName:@"SecondViewController" 
+								   bundle:[NSBundle mainBundle]];
+	tempTBC.viewControllers = localControllersArray;
+	[localControllersArray release];
+	self.tabBar = tempTBC;
+	[tempTBC release];
+	
+	[window addSubview:self.tabBar.view];
+	[window makeKeyAndVisible];
 }
 
 - (void)didReceiveMemoryWarning {
